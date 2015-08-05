29c923fe7b8a0ea515450ae4ac09a5b3d31020a2
Smurf Dad <smurfdad@smurfdad.es>
2015-08-05 12:42:22 +0200

    Configuracion OpenShift

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
 var http = require('http');
-var PORT = 80;
 http.createServer(function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/plain'});
 	res.end('Hello World1\n');
-}).listen(PORT);
-console.log('Server running on '+PORT+ " port");
+}).listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", server_port " + server_port )
+});
